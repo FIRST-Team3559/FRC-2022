f7df9c86370e473927ca6811595b2cd0a089d690
Fred Routson <froutson@gmail.com>
2022-01-17 19:49:48 -0500

    DriveBaseSubsystem additions

--- src/main/java/frc/robot/subsystems/DriveBaseSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveBaseSubsystem.java
@@ -4,15 +4,38 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.command.Subsystem;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
 
 /** Add your docs here. */
-public class DriveBaseSubsystem extends Subsystem {
+public class DriveBaseSubsystem extends SubsystemBase {
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
 
+  // Motor controllers Left
+  public CANSparkMax mc_leftFront = new CANSparkMax(Constants.FL_MOTOR_CONTROLLER_ID, MotorType.kBrushless);
+  public CANSparkMax mc_leftRear = new CANSparkMax(Constants.RL_MOTOR_CONTROLLER_ID, MotorType.kBrushless);
+  MotorControllerGroup mcg_left = new MotorControllerGroup(mc_leftFront, mc_leftRear);
+  // Motor controllers Right
+  public CANSparkMax mc_rightFront = new CANSparkMax(Constants.FR_MOTOR_CONTROLLER_ID, MotorType.kBrushless);
+  public CANSparkMax mc_rightRear = new CANSparkMax(Constants.RR_MOTOR_CONTROLLER_ID, MotorType.kBrushless);
+  MotorControllerGroup mcg_right = new MotorControllerGroup(mc_rightFront, mc_rightRear);
+
+  // instantiate a new differential drive object
+  public DifferentialDrive drivetrain = new DifferentialDrive(mcg_left, mcg_right);
+
+  public void manualDrive(double getLeftStick, double getRightStick) {
+    drivetrain.tankDrive(getLeftStick, getRightStick);
+  }
+
   @Override
-  public void initDefaultCommand() {
+  public void periodic() {
     // Set the default command for a subsystem here.
     // setDefaultCommand(new MySpecialCommand());
   }
