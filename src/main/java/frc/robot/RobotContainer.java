6950a205487f7d4dd36c3107a9e4a1eeaf620c42
alexarvin6 <97808017+alexarvin6@users.noreply.github.com>
2022-01-20 14:48:12 -0500

    Update RobotContainer.java

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,8 +6,6 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.POVButton;
 import edu.wpi.first.wpilibj.Joystick;
@@ -26,10 +24,6 @@ public class RobotContainer {
   public static POVButton povRight = new POVButton(stick0, 90);
   public static POVButton povBackward = new POVButton(stick0, 180);
   public static POVButton povLeft = new POVButton(stick0, 270);
-  
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
-
-  private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
