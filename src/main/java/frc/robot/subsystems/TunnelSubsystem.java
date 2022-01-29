b92e1f63385a9c41dd61c2b37d5fd77069512cb4
alexarvin6 <97808017+alexarvin6@users.noreply.github.com>
2022-01-29 14:56:48 -0500

    Update TunnelSubsystem.java

--- src/main/java/frc/robot/subsystems/TunnelSubsystem.java
+++ src/main/java/frc/robot/subsystems/TunnelSubsystem.java
@@ -1,6 +1,7 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class TunnelSubsystem extends SubsystemBase {
   public Spark bottomTunnelMotor = new Spark(Constants.bottomTunnelChannel);
