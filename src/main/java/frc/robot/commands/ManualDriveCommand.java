83b374113d1f85cc7edd4215a94a7379bff027b3
alexarvin6 <97808017+alexarvin6@users.noreply.github.com>
2022-02-06 11:23:27 -0500

    Update ManualDriveCommand.java

--- src/main/java/frc/robot/commands/ManualDriveCommand.java
+++ src/main/java/frc/robot/commands/ManualDriveCommand.java
@@ -19,19 +19,21 @@ public class ManualDriveCommand extends CommandBase {
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+  .getPOV(
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+  
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
-
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return false;
+  public void end(boolean interrupted) {
+    if (ManualDriveCommand.withInterrupt(true)) {
+      ManualDriveCommand.cancel;
+    }
   }
 }
